94d1d316e475f1807a4819db8e008d0e058ee8da
jjgsherwood <jonnegoedhart@gmail.com>
2018-06-07 14:36:47 +0200

    version1 dataframe

--- test_jonne.py
+++ test_jonne.py
@@ -73,6 +73,25 @@ def get_values_column(df, column_name, value):
     """
     return df.loc[df[column_name] == value]
 
+def consecutive_dates(serie, gap = 0):
+    """
+    Dit genereerd de lengte van de tijds intervallen van een pandas.serrie.
+    Met gap kan je toelaten dat er eventueel x aantal maanden tussen de data punten zit.
+    """
+    consecutive_lst = []
+    n = 1
+    date = list(map(int, serie.iloc[0].split("-")))
+    for x in serie.iloc[1:]:
+        tmp = list(map(int, x.split("-")))
+        if 0 <= (tmp[0] - date[0]) * 12 + tmp[1] - date[1] <= gap + 1:
+            n += 1
+        else:
+            consecutive_lst.append(n)
+            n = 1
+        date = tmp
+    consecutive_lst.append(n)
+    return consecutive_lst
+
 def save_to_csv(df, filename):
     """
     Save dataframe to csv.
@@ -90,13 +109,6 @@ def remove_Curr(df):
     verwijder currentie somaliland shilling
     """
     return df.loc[df[CURR] != 'Somaliland Shilling']
-
-def calc_diff(df, n = 1):
-    """
-    geeft de diverentiaal terug van de dataset.
-    """
-
-    return np.diff(df[PRICE], n, axis = 0)
 
 def change_duplicate_city(df):
     """
@@ -120,34 +132,25 @@ def remove_Region(df):
     """
     return df.drop(REGION, axis=1)
 
-def remove_less_then(df, m = 12):
+def remove_less_then(df, m = 12, gap = 0):
     """
     Dit verwijderd alle data producten per markt/stad dat hebben minder dan m data punten.
     """
-    return df.groupby([CITY, PROD]).filter(lambda x: len(x) > m)
+    return df.groupby([CITY, PROD]).filter(lambda x: len(x) > m and max(consecutive_dates(x.eval(DATE), gap = gap)) > m)
 
-def consecutive_dates(serie, cap = 1):
-    n = 0
-    # date = serie[0].split("-")
-    # for x in serie[1:]:
-    #     tmp = x.split("-")
-    #     tmp[0] date[0]
+def calc_diff(df, n = 1):
+    """
+    geeft de diverentiaal terug van de dataset.
+    """
 
-
+    return np.diff(df[PRICE], n, axis = 0)
 
 if __name__ == "__main__":
     df = pd.read_csv('WFPVAM_FoodPrices_compressed.csv')
-    unique_per_cat(df)
-    df2 = remove_less_then(remove_Region(change_duplicate_city(remove_Curr(join_YEAR_month(df)))))
-    # dates = pd.to_datetime(df2.eval(DATE))
-    # print(dates[1]-dates[0])
-    # print(consecutive_date(df2.eval(DATE)[:12]))
+    # unique_per_cat(df)
+    df2 = remove_less_then(remove_Region(change_duplicate_city(remove_Curr(join_YEAR_month(df)))), gap = 2)
     unique_per_cat(df2)
-
-
-
-
-
+    save_to_csv(df2, 'WFPVAM_FoodPrices_version1.csv')
 
 
 
